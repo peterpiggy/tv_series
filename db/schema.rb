7cc53ca2ad5d7d103e37d573278447289543d8cc
Павел <pavel@MacBook-Pro-Pavel.local>
2016-05-03 13:14:16 +0300

    Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160501062414) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tv_series", force: :cascade do |t|
     t.string   "name",              default: "", null: false
     t.integer  "season",            default: 1,  null: false
