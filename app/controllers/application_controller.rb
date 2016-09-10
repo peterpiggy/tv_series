a819e369f487c4286145341e39a551c5a42863bd
Pavel <drumbron@icloud.com>
2016-09-10 20:24:12 +0300

    Change torrent link to new kickass mirror

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-  TORRENT_DOMAIN = 'http://extratorrent.cc/search/?search='
+  TORRENT_DOMAIN = 'http://kickass.cd/search.php?q='
 end
